fd73ebf8d329204de941cd1f06bda9487c2c787a
vuceta <73024357+vuceta@users.noreply.github.com>
2021-01-31 09:38:13 +0100

    Add files via upload

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,15 +1,12 @@
-//add and remove
+// add button and remove
 
 $(function() {
-    var listState = 'add';
-    var editingTodoId = null;
     $('.add').on('click', function() {
-      if(listState === 'add') {
         let list = $('.input').val();
         if (list !== '') {
-             $('ul').append("<li><span><i class='fa fa-trash-o'></i></span>" + list  + "</li>");
+             $('ul').append("<li><span><i class='fa fa-trash-o'></i></span>" + list + "</li>");
              $('.input').val(''); 
-             $("ul").on('click', 'span', function () {
+             $('ul').on('click', 'span', function () {
                  $(this).closest("li").fadeOut(500,function() {
                   $(this).remove();
                   $('.input').val('');
@@ -17,18 +14,24 @@ $(function() {
                });
              return false;
          }
-      } else if (listState === 'edit') {
-       ///blabla 
-      }
-      
     });
-    $(document).on('dblclick', 'li', function() {
-        listState = "edit";
-        $('.input').val($(this).text());
-        $('.add').html('Save')
-      });
-});
 
+    // edit and save button
+
+    let indexTask;
+    $(document).on('dblclick', 'li', function() {
+      indexTask = $(this).index()
+      $('.input').val($(this).text());
+      $('.add').html('Save')
+    });
+    $('.add').on('click', function() {
+      if ($(this).text() == "Save") {
+        $("li:eq(" + indexTask + ")").html('');
+        $('.add').html('Add');
+        $('.input').val('')
+      }
+    })
+  });
 
 
 
