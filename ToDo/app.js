a6f221b262b823c9b73959b112481df40389257c
vuceta <73024357+vuceta@users.noreply.github.com>
2021-02-02 20:55:08 +0100

    Add files via upload

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,37 +1,39 @@
 // add button and remove
-
 $(function() {
-    $('.add').on('click', function() {
-        let list = $('.input').val();
-        if (list !== '') {
-             $('ul').append("<li><span><i class='fa fa-trash-o'></i></span>" + list + "</li>");
-             $('.input').val(''); 
-             $('ul').on('click', 'span', function () {
-                 $(this).closest("li").fadeOut(500,function() {
-                  $(this).remove();
-                  $('.input').val('');
-                 });
-               });
-             return false;
-         }
-    });
-
-    // edit and save button
-
-    let indexTask;
-    $(document).on('dblclick', 'li', function() {
-      indexTask = $(this).index()
-      $('.input').val($(this).text());
-      $('.add').html('Save')
-    });
-    $('.add').on('click', function() {
-      if ($(this).text() == "Save") {
-        $("li:eq(" + indexTask + ")").html('');
-        $('.add').html('Add');
-        $('.input').val('')
+  let listState = 'add';
+  $('.add').on('click', function() {
+    if (listState === 'add') {
+      let list = $('.input').val();
+      if (list !== '') {
+        $('ul').append("<li><span><i class='fa fa-trash-o'> </i></span>" + list + "</li>");
+        $('.input').val('');
+        $('ul').on('click', 'span', function() {
+          $(this).closest('li').fadeOut(500, function() {
+            $(this).remove();
+            $('.input').val('');
+          });
+        });
       }
-    })
+    } else if (listState === 'edit') {
+      $('.add').on('click', function() {
+        listState = 'add';
+        if ($(this).text() == 'Save') {
+          $("li:eq(" + indexTask + ")").html("<span><i class='fa fa-trash-o'> </i></span>" + $('.input').val())
+          $('.add').html('Add');
+          $('.input').val('')
+        }
+      })
+    };
+    return false;
   });
+  let indexTask;
+  $(document).on('click', 'li', function() {
+    listState = 'edit';
+    indexTask = $(this).index()
+    $('.input').val($(this).text().trim());
+    $('.add').html('Save')
+  });
+});
 
 
 
